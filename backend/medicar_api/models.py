5f9cb375d955e5be054f75222aa097d6c5bba406
Jose Almeida <almeidajcr90@gmail.com>
2020-12-10 19:46:28 -0300

    feat: add Doctor model

--- backend/medicar_api/models.py
+++ backend/medicar_api/models.py
@@ -12,3 +12,15 @@ class Specialty(models.Model):
     class Meta:
         db_table = 'specialty'
 
+class Doctor(models.Model):
+    """
+    The Doctor class represents the model for the Doctor table.
+    """
+    name = models.CharField(max_length=60)
+    crm = models.IntegerField()
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=60, null=True)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
