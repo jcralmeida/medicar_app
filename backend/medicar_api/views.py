89604668bf2f1b9c57f18886358939c9b4b6c447
Jose Almeida <almeidajcr90@gmail.com>
2020-12-10 13:27:30 -0300

    feat: add a get_specialty method to the app

--- backend/medicar_api/views.py
+++ backend/medicar_api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from medicar_api.models import Specialty
+from medicar_api.serializers import SpecialtySerializer
+from medicar_api.utils import prepare_response
+
+
+@api_view(['GET'])
+def get_specialty(request):
+    """
+    Get the specialty that has been already registered.
+
+    :param request: the request received from the user.
+    :return: A response in a json format.
+    """
+
+    specialty_name_filter = request.query_params.get("name")
+
+    if(specialty_name_filter):
+        retrieved_specialty = Specialty.objects.filter(name=specialty_name_filter).all()
+    else:
+        retrieved_specialty = Specialty.objects.filter().all()
+
+    serialized_specialty = SpecialtySerializer(retrieved_specialty, many=True)
+
+    json_response = prepare_response(serialized_specialty)
+    return json_response
+
