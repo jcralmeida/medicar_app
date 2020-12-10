41e28700d364045707fbee8e817c6adfa1604062
Jose Almeida <almeidajcr90@gmail.com>
2020-12-10 13:26:29 -0300

    feat: create the Specialty model

--- backend/medicar_api/models.py
+++ backend/medicar_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Specialty(models.Model):
+    """
+    The Specialty represents the model for Specialty table.
+    """
+
+    name = models.CharField(max_length=60)
+
+    class Meta:
+        db_table = 'specialty'
+
