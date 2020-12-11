137f302666c5313bbb8fbad3e4ab630102ddedd1
Jose Almeida <almeidajcr90@gmail.com>
2020-12-11 07:02:14 -0300

    feat: register Schedule to admin page

--- backend/medicar_api/admin.py
+++ backend/medicar_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from medicar_api.models import Specialty, Doctor
+from medicar_api.models import Specialty, Doctor, Schedule
 
 admin.site.register(Specialty)
 admin.site.register(Doctor)
+admin.site.register(Schedule)
