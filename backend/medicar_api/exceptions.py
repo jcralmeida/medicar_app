d23fdf399d89559e1af399434ebf3f8b5a24038b
Jose Almeida <almeidajcr90@gmail.com>
2020-12-12 11:03:01 -0300

    feat: add HourUnavailable exception

--- backend/medicar_api/exceptions.py
+++ backend/medicar_api/exceptions.py
@@ -18,3 +18,12 @@ class InvalidDay(APIException):
     default_detail = 'Não é possivel marcar uma consulta para um dia no passado.'
     default_code = 'Bad Request'
 
+
+class HourUnavailable(APIException):
+    """
+        Exception raised when the client tries to set an appointment in an hour that
+        has already been taken.
+    """
+    status_code = 400
+    default_detail = 'Este horário não está disponivel. Por favor, escolha outro'
+    default_code = 'Bad Request'
