bfefb1e018c9ed095706dc83c1c931fe8e885aeb
Jose Almeida <almeidajcr90@gmail.com>
2020-12-12 11:03:48 -0300

    fix: refactor the response pattern by removing the word content from it

--- backend/medicar_api/utils.py
+++ backend/medicar_api/utils.py
@@ -9,9 +9,8 @@ def prepare_response(serialized_object):
     :param serialized_object: a serialized object.
     :return: A response in a json format.
     """
-    json_response = JsonResponse({
-        'content': serialized_object.data
-    },
+    json_response = JsonResponse(
+        serialized_object.data,
         status=status.HTTP_200_OK)
 
     return json_response
