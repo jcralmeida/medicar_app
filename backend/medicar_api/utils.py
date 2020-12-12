5ea5937e13beb57456fe313647b9908b6f9546c1
Jose Almeida <almeidajcr90@gmail.com>
2020-12-12 15:38:33 -0300

    feat: add prepare_doctor_query_parameters method to utils

--- backend/medicar_api/utils.py
+++ backend/medicar_api/utils.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.response import Response
 
 
 def prepare_response(serialized_object):
@@ -14,3 +15,26 @@ def prepare_response(serialized_object):
         status=status.HTTP_200_OK)
 
     return json_response
+
+
+def prepare_list_response(serialized_object, status):
+    """
+    Prepare the response as a list that will be shown to the user.
+
+    :param serialized_object: a serialized object.
+    :return: A response as list.
+    """
+    return Response(
+        list(serialized_object.data),
+        status=status)
+
+
+def prepare_doctor_query_parameters(request):
+    params = request.query_params
+    query_params_dict = dict()
+    for key in params.dict().keys():
+        if key == "search":
+            query_params_dict["nome"] = params.dict().get('search')
+        elif key == 'especialidade':
+            query_params_dict['especialidade'] = params.dict().get('especialidade')
+    return query_params_dict
