4a600232cedd37d76171b922b3e3edb3c67115ef
Jose Almeida <almeidajcr90@gmail.com>
2020-12-16 20:26:22 -0300

    feat: remove DEFAULT_PERMISSION_CLASSES from settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,9 +89,6 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
