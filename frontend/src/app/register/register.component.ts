4e0a2a095c89a420c3899ff4a30bcba440e5c0b6
Jose Almeida <almeidajcr90@gmail.com>
2020-12-17 09:00:43 -0300

    feat: implement a logic to show a basic spinner on register.component.html

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,20 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) { }
+
+  showSpinner = false;
 
   ngOnInit(): void {
   }
 
   onRegister(form) {
-    this.authService.register(form).subscribe();
+    this.authService.register(form).subscribe(() => {
+      this.showSpinner = true;
+      setTimeout(() => {
+        this.showSpinner = false;
+          this.router.navigateByUrl('/login');
+      }, 1000);
+    });
   }
 }
