b72b08ce01af1b1d8f960d49105b4b44760d605a
Jose Almeida <almeidajcr90@gmail.com>
2020-12-16 22:31:24 -0300

    fix: refactor the logic to login to the app

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,25 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private route: Router) { }
 
   ngOnInit(): void {
   }
 
   onLogin(value) {
-    this.authService.register(value).subscribe()
+    this.authService.login(value).subscribe(
+       data => {
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('token', data.token);
+        this.route.navigateByUrl('/list');
+      },
+      error => {
+        this.errorMessage = error.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
-
 }
