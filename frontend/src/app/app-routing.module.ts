57ac4eee266bbf57adfa579c7c79c04a1bcc20af
Jose Almeida <almeidajcr90@gmail.com>
2020-12-16 22:29:37 -0300

    feat: add AuthGuard module to app routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RegisterComponent} from "./register/register.component";
-import {LoginComponent} from "./login/login.component";
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { ListAppointmentsComponent } from './list-appointments/list-appointments.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'list', component: ListAppointmentsComponent}
+  {path: 'list', component: ListAppointmentsComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
